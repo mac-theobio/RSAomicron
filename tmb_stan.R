373a29572b6736f479d460206c18e84b2d5e7da1
Ben Bolker <bbolker@gmail.com>
2021-12-20 20:51:15 -0500

    a little more tmbstan

--- tmb_stan.R
+++ tmb_stan.R
@@ -2,10 +2,19 @@ library(shellpipes)
 rpcall("tmb_ci.Rout tmb_ci.R tmb_fit.rds")
 library(TMB)
 library(tmbstan)
+library(rstan)
 
 fit <- rdsRead()
 loadEnvironments()
 dyn.load(dynlib(get_tmb_file(fit)))
-tt <- tmbstan(fit, chains = 2, iter = 500)
+op <- options(mc.cores = min(4, parallel::detectCores() - 1))
+tt <- tmbstan(fit, chains = 4, iter = 1000, seed = 101)
+summary(tt)
 
+if (FALSE) {
+    library(shinystan)
+    launch_shinystan(tt)
+}
+
+options(op)
 rdsSave(tt)
