892b56e2839f2d9c540dbfec0565f372d4dae7a5
Jonathan Dushoff <dushoff@mcmaster.ca>
2021-12-22 17:55:10 -0500

    Some confusion

---
+++ tmbfit.R
@@ -0,0 +1,21 @@
+library(shellpipes)
+library(TMB)
+library(dplyr)
+
+loadEnvironments()
+soLoad()
+
+## fit (using all defaults)
+tt <- tmb_fit(data = rdsRead(),
+	      two_stage = TRUE,
+	      start = list(log_deltar = log(0.1),
+			   lodrop = -4, logain = -7),
+	      upper = list(log_theta = 20),
+	      lower = NULL,
+	      priors = list(logsd_logdeltar =
+		prior_params(lwr=log(0.01), upr=log(0.3))),
+	      map = list(),  ## no fixed params
+	      debug_level = 0
+)
+
+rdsSave(tt)
