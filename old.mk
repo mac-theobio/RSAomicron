b2795346cf81c216d7ed041cfe2c87ab76c6bdb7
Jonathan Dushoff <dushoff@mcmaster.ca>
2021-12-22 17:53:40 -0500

    new file:   old.mk

---
+++ old.mk
@@ -0,0 +1,11 @@
+
+tmb_fit.Rout: tmb_fit.R btfake.sgts.rds logistic.so tmb_funs.rda
+
+tmb_eval.Rout: tmb_eval.R tmb_fit.rds logistic.so tmb_funs.rda
+
+tmb_ci.Rout: tmb_ci.R tmb_fit.rds tmb_funs.rda logistic.so
+
+tmb_ci_plot.Rout: tmb_ci_plot.R tmb_ci.rds
+
+tmb_stan.Rout: tmb_stan.R tmb_fit.rds tmb_funs.rda
+
