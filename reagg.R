e6c847bd4abd53cb9c373938094a9266acbf08fb
wzmli <wzmichael.li@gmail.com>
2022-01-27 22:24:00 -0500

    adding a quit

--- reagg.R
+++ reagg.R
@@ -8,11 +8,14 @@ ts <- (rdsRead())
 
 print(summary(ts %>% mutate_if(is.character, as.factor)))
 
+quit()
+
 ts <- (ts
 	%>% transmute(NULL
 		, prov, date
 		, time = as.numeric(date - zeroDate)
-		, reinf = (infection == "reinfection")
+#		, reinf = (infection == "reinfection")
+		, reinf
 		, omicron = SGTF
 		, delta = nonSGTF
 	)
